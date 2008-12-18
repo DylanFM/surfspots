53506d1e6b9974794558c9e72459a6840b63d7a7
DylanFM <dylan.fm@gmail.com>
2008-12-18 23:51:11 +1030

    Fixed up dependencies for latest merb and dm versions. Added datamapper serialiser dependency.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.3"
-dm_gems_version   = "0.9.7"
+merb_gems_version = "1.0.6.1"
+dm_gems_version   = "0.9.8"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
@@ -20,4 +20,5 @@ dependency "dm-aggregates", dm_gems_version
 dependency "dm-migrations", dm_gems_version   
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version        
-dependency "dm-validations", dm_gems_version
+dependency "dm-validations", dm_gems_version  
+dependency "dm-serializer", dm_gems_version
